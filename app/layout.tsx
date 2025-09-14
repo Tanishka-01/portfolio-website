af43e8eeb86d5d77d39c3219d0b626b88bd3fc4c
Tanishka Wani <tanishkawani@gmail.com>
2025-09-14 16:18:38 -0400

    updated favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <link
           rel="icon"
-          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><defs><linearGradient id='grad' x1='0%25' y1='0%25' x2='100%25' y2='100%25'><stop offset='0%25' style='stop-color:%23f1bfd7;stop-opacity:1' /><stop offset='25%25' style='stop-color:%23f0b8f4;stop-opacity:1' /><stop offset='50%25' style='stop-color:%23fed9a6;stop-opacity:1' /><stop offset='75%25' style='stop-color:%23feccbb;stop-opacity:1' /><stop offset='100%25' style='stop-color:%23602e9e;stop-opacity:1' /></linearGradient></defs><circle cx='50' cy='50' r='50' fill='url(%23grad)'/></svg>"
+          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><circle cx='50' cy='50' r='50' fill='%23f1bfd7'/></svg>"
         />
       </head>
       <body className={inter.className}>
